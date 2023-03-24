145e19a2f5ebfae89bc4dd25ae5aa1b82922e9cd
Eliot Gronstal <eliot.lauren@gmail.com>
2023-03-24 14:01:01 -0700

    add Order class to place Order for treats.

---
+++ Bakery/Models/OrderTreat.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bakery.Models
+{
+  public class Order
+    {
+        public int TreatId { get; set; }
+        [Required(ErrorMessage = "The kind of treat cannot be left blank.")]
+        public string Kind { get; set; }
+        public List<FlavorTreat> JoinFlavorTreat { get;}
+        public ApplicationUser User { get; set; } 
+    }
+}
